ff43c2a3a5955338ad2706eeca203087fd4ce42b
Student Anish <anishdhene3124@gmail.com>
2026-05-08 18:26:40 +0530

    Fixed Navbar import

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import "../styles/Navbar.css"
 
 function Navbar() {
     return(
-        <nav className="navbar">
+        <nav className="Navbar">
             <h1>ExpenseAI</h1>
         </nav>
     )
